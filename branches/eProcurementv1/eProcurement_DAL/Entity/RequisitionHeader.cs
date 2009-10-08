cfd30dce876cf796bcd012c18acc58c9ef43c446
eieithu.mdy@gmail.com <eieithu.mdy@gmail.com@4847b3fc-9f72-11de-b2a8-a7b03212e241>
2009-10-08 14:52:19 +0000

    created by ei ei

---
+++ branches/eProcurementv1/eProcurement_DAL/Entity/RequisitionHeader.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eProcurement_DAL
+{
+    [Serializable]
+    public class RequisitionHeader
+    {
+        string requisitionNumber;
+        public string RequisitionNumber
+        {
+            get { return requisitionNumber; }
+            set { requisitionNumber = value; }
+        }
+
+        string documentType;
+        public string DocumentType
+        {
+            get { return documentType; }
+            set { documentType = value; }
+        }
+
+
+        string requisitionCategory;
+        public string RequisitionCategory
+        {
+            get { return requisitionCategory; }
+            set { requisitionCategory = value; }
+        }
+
+   
+        Nullable<long> requisitionDate;
+        public Nullable<long> RequisitionDate
+        {
+            get { return requisitionDate; }
+            set { requisitionDate = value; }
+        }
+
+        string status;
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }
+
+        string releaseStatus;
+        public string ReleaseStatus
+        {
+            get { return releaseStatus; }
+            set { releaseStatus = value; }
+        }
+
+        Nullable<long> releaseDate;
+        public Nullable<long> ReleaseDate
+        {
+            get { return releaseDate; }
+            set { releaseDate = value; }
+        }
+        
+    }
+}
+
